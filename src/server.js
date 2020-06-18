fcf2670f8cbede46ecbedca3d0f6aa126eb5ece9
Wanderson-Timoteo <65818787+Wanderson-Timoteo@users.noreply.github.com>
2020-06-18 13:47:19 -0400

    instalação nodemon

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 const express =  require("express")
 const server =  express()
 
+// Configurar rotas da minha aplicação
+// página inicial
+// req: pergunta ao servidor
+// res: resposta do servidor
+server.get("/", (req, res) => {
+    res.send("Cheguei aqui")
+}) 
+
 // ligar o servidor
 server.listen(3000)
 
