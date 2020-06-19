4d1c3fdc7a0bb3b004ddb647e10b010b46ead54c
Wanderson-Timoteo <65818787+Wanderson-Timoteo@users.noreply.github.com>
2020-06-19 13:45:37 -0400

    continuação conf. server

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
 const express =  require("express")
 const server =  express()
 
+// Configurar pasta publica
+server.use(express.static("public"))
+
 // Configurar rotas da minha aplicação
 // página inicial
 // req: pergunta ao servidor
 // res: resposta do servidor
 server.get("/", (req, res) => {
-    res.send("Cheguei aqui")
+    res.sendFile(__dirname + "/views/index.html")
 }) 
+
+server.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html")
+}) 
+
 
 // ligar o servidor
 server.listen(3000)
